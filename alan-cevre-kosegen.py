3cfd6a130c6ce1ebe736007bba88907bd434ef38
Cagla Nur Sorkun <caglasorkun@gmail.com>
2023-02-05 18:28:42 +0300

    alan-cevre-kosegen python dosyamı commitledim

---
+++ alan-cevre-kosegen.py
@@ -0,0 +1,9 @@
+# Çağla Nur Sorkun
+a=float(input("Uzun Kenar Değerini Giriniz: "))
+b=float(input("Kısa Kenar Değerini Giriniz: "))
+alan=a*b
+çevre=2*(a+b)
+köşegen_uzunluğu=(a**2+b**2)**0.5
+print("Dikdörtgenin alanı: ",alan)
+print("Dikdörtgenin çevresi: ",çevre)
+print("Dikdörtgenin köşegen uzunluğu: ",köşegen_uzunluğu)
